90989a52a262aaf4763a717d8c1df85f73f18bdc
Aaron McLaughlin <aaron.mclaughlin@learningpool.com>
2017-07-07 14:32:17 +0100

    Issue/#6/ Resolved window.top issue when content loads form a different domain

--- js/adapt-contrib-ios-frameset.js
+++ js/adapt-contrib-ios-frameset.js
@@ -6,10 +6,12 @@ $(function() {
     // Detect if we're in a FRAME or IFRAME
     if (window.frameElement && (window.frameElement.nodeName == "FRAME" || window.frameElement.nodeName == "IFRAME")) {
 
-        if (!window.top.document.querySelector("meta[name=viewport]")) {
-            $("head", window.top.document).append('<meta name="viewport" content="initial-scale=1.0, user-scalable=yes">');
-        } else {
-            $("meta[name='viewport']", window.top.document).attr("content", "initial-scale=1.0, user-scalable=yes");
+        if (window.top) {
+            if (!window.top.document.querySelector("meta[name=viewport]")) {
+                $("head", window.top.document).append('<meta name="viewport" content="initial-scale=1.0, user-scalable=yes">');
+            } else {
+                $("meta[name='viewport']", window.top.document).attr("content", "initial-scale=1.0, user-scalable=yes");
+            }
         }
 
         $('html').addClass('frameset');
